d85287a0bf9f262eddb2da6131d7ee9324cb5e87
andreamaluta <andreamaluta@gmail.com>
2025-05-13 14:41:04 +0200

    [UPD] create the base card

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,31 @@ function App() {
   const [actress, setActress] = useState([]);
 
   const fetchActress = () => {
-    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => setActress(resp.data.name))
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => setActress(resp.data))
   }
 
   useEffect(() => (fetchActress()), []);
 
   return (
     <>
-      <h1>Hello world</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card d-flex flex-row">
+              <div className="card-img">
+                <p>immagine</p>
+              </div>
+              <div className="card-body ">
+                <div className="name mb-3">Lorem</div>
+                <div className="data mb-3">lorem</div>
+                <div className="nation mb-3">lorem</div>
+                <div className="bio mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, assumenda. At veritatis quo, voluptates a ea molestias asperiores beatae expedita impedit vero nihil commodi praesentium alias nobis est soluta accusamus?</div>
+                <div className="recognition">lorem</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
