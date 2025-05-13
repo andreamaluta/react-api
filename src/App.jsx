f0ffa8c5bb07d87df8cdfe838f5a5a254bf89aaa
andreamaluta <andreamaluta@gmail.com>
2025-05-13 14:15:26 +0200

    [UPD] made the axios call

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import axios from 'axios'
 
 function App() {
 
+  const [actress, setActress] = useState([]);
+
+  const fetchActress = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => console.log(resp.data.name))
+  }
+
   return (
     <>
       <h1>Hello world</h1>
